1b252a26003123370a1fdd32a4a995e04fea117a
OHHAKO <l36h7129@gmail.com>
2019-12-25 15:48:11 +0900

    Get try number from user

--- src/main/java/domain/Racing.java
+++ src/main/java/domain/Racing.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 public class Racing {
 	private Car cars[];
 	private String carNames[];
-	int length;
+	private int k;
+	Scanner sc;
 	
 	public Racing() {
 		init();
@@ -12,18 +13,26 @@ public class Racing {
 	
 	void init() {
 		MakeCar();
+		MakeTryNumber();
 	}
 	
 	void MakeCar() {
-		Scanner sc = new Scanner(System.in);
+		sc = new Scanner(System.in);
 		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
 		String names = sc.next();
 		carNames = names.split(",");
 		cars = new Car[carNames.length];
 		
-		for(int i=0 ; i<carNames.length ; i++) {
+		for(int i=0 ; i<carNames.length ; i++) 
 			cars[i] = new Car(carNames[i]);
-		}
+		
 	}
+	
+	void MakeTryNumber() {
+		System.out.println("시도할 회수는 몇회인가요?");
+		this.k = sc.nextInt();
+	}
+	
+	
 	
 }

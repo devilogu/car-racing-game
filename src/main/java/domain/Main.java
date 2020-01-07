0c0ec9ec74de653f09ef222f405871c21a1d9cb8
Sujeong Jang <56670564+sujeong-jang@users.noreply.github.com>
2020-01-07 19:51:04 +0900

    update : remove unnecessary code

--- src/main/java/domain/Main.java
+++ src/main/java/domain/Main.java
@@ -6,7 +6,6 @@ public class Main {
 	public static void main(String[] args) {
 		String nameList;
 		int count = 0; 
-		Scanner sc = new Scanner(System.in);
 		
 		RacingGame game = new RacingGame();
 		
